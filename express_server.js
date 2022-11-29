0273bade1ec5474df005a3ab8e6b6b96c26df0e8
ruilinqi <ruilinqi7@gmail.com>
2022-11-29 00:42:15 +0000

    Add additional endpoints

--- express_server.js
+++ express_server.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase); // => {"b2xVn2":"http://www.lighthouselabs.ca","9sm5xK":"http://www.google.com"}
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
